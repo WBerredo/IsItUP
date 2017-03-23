f3418f835077960876b82f0651969b1763a228c8
Wesley Berrêdo <wberredo@gmail.com>
2017-03-23 15:45:43 -0400

    Enable chatbot at groups

--- index.js
+++ index.js
@@ -13,22 +13,30 @@ let telegram = new TelegramBot(token, {polling: true});
 // possibilities Regex
 let commandString  = "\/verify"
 let urlString = "(http([s])?:\\/\\/){0,1}(www\\.){0,1}[a-zA-Z0-9\\.\\-]+\\.[a-zA-Z]{2,5}[\\.]{0,1}"
+let botUsernameCallString = "^@Is_it_UPBot\\s";
 
 let startRegex = /\/start/;
 let urlRegex = new RegExp("^" + urlString);
 let verifyUrlRegex = new RegExp(commandString + "\\s" + urlString);
 let justVerifyRegex = new RegExp(commandString + "$");
+let usernameCallRegex = new RegExp(botUsernameCallString);
+let usernameCallLinkRegex = new RegExp(botUsernameCallString + urlString);
 
-console.log("Running!")
+// Messages
+let didntUnderstand = "Sorry, I didn't understand what you said! Try to pass an URL :D";
+let verifyHowToUse = "To use /verify command you have to pass an URL. Ex: /verify telegram.org";
+
+console.log("Running! \u{1F604}")
 
 // first message
 telegram.onText(startRegex, (msg, match) => {
     username = msg.from.first_name;
-    firstMessage = `Hi ${username}, I'm the Is it UP bot, my mission is tell you if any website is up or down.`;
-    secondMessage = `You can use the command /verify telegram.org/ or just type your url to verify the current status.`;
 
-    telegram.sendMessage(msg.chat.id, firstMessage);
-    telegram.sendMessage(msg.chat.id, secondMessage);
+    let welcomeFirstMsg  = `Hi ${username}, I'm the Is it UP bot, my mission is tell you if any website is up or down.`;
+    let welcomeSecondMsg = `You can use the command /verify telegram.org/ or just type your url to verify the current status.`;
+
+    telegram.sendMessage(msg.chat.id, welcomeFirstMsg);
+    telegram.sendMessage(msg.chat.id, welcomeSecondMsg);
 });
 
 // verify urls
@@ -40,18 +48,30 @@ telegram.onText(verifyUrlRegex, (msg, match) => {
 
 // just verify
 telegram.onText(justVerifyRegex, (msg) => {
-    let message = "To use /verify command you have to pass an URL. Ex: /verify telegram.org";
-    telegram.sendMessage(msg.chat.id, message);
+    telegram.sendMessage(msg.chat.id, verifyHowToUse);
 })
 
 // not found
-telegram.onText(/.*/, (msg, match) => {
-    if(!match[0].match(urlRegex)
-    && !match[0].match(verifyUrlRegex)
-    && !match[0].match(startRegex)
-    && !match[0].match(justVerifyRegex) ) {
-        console.log("error: " + match[0]);
-        telegram.sendMessage(msg.chat.id, "Sorry, I didn't understand what you said! Try to pass an URL :D");
+telegram.on('message', msg => {
+    let textMessage = msg.text;
+    let chatId = msg.chat.id;
+
+    if(textMessage != undefined
+    && !textMessage.match(urlRegex)
+    && !textMessage.match(verifyUrlRegex)
+    && !textMessage.match(startRegex)
+    && !textMessage.match(justVerifyRegex) ) {
+        if(usernameCallRegex.exec(textMessage)) {
+              if(match = usernameCallLinkRegex.exec(textMessage)) {
+                  match[0] = match[0].split(' ')[1];
+                  verify(msg, match);
+              } else {
+                  telegram.sendMessage(chatId, didntUnderstand);
+              }
+        } else if(msg.chat.type!='group') {
+            console.log("error: " + textMessage);
+            telegram.sendMessage(chatId, didntUnderstand);
+        }
     }
 });
 
@@ -72,8 +92,8 @@ function verify(msg, match) {
     console.log("URL: " + url);
 
     request.get(url, res => {
-        telegram.sendMessage(msg.chat.id, url + " is up :D");
+        telegram.sendMessage(msg.chat.id, url + " is up \u{1F604}");
     }).on('error', e => {
-        telegram.sendMessage(msg.chat.id, url + " is down :(");
+        telegram.sendMessage(msg.chat.id, url + " is down \u{1F614}");
     });
 }
