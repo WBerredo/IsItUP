d90708ca866124190a5d0507f622667ea52354a3
Wesley Berrêdo <wberredo@gmail.com>
2017-03-24 14:13:04 -0400

    Notify when status code is between 400 and 600

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ let telegram = new TelegramBot(token, {polling: true, onlyFirstMatch: true});
 
 // possibilities Regex
 let commandString  = "\/verify"
-let urlString = "(http([s])?:\\/\\/){0,1}(www\\.){0,1}[a-zA-Z0-9\\.\\-]+\\.[a-zA-Z]{2,5}[\\.]{0,1}"
+
+// Adapted from https://gist.github.com/dperini/729294#file-regex-weburl-js
+let urlString = "^(?:(http(s)?|ftp):\\/\\/)?(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)" +
+                "(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})"  +
+                "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+                "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5]))" +
+                "{2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)" +
+                "*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*" +
+                "[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))\\.?)" +
+                "(?::\\d{2,5})?(?:[/?#]\\S*)?";
 let botUsernameCallString = "^@Is_it_UPBot\\s";
 
 let startRegex = /\/start/;
@@ -83,17 +92,29 @@ function verify(msg, match) {
 
     if(url.match(justVerifyRegex)) return;
 
+//  verify if its https or doesn`t have protocol
     if(match[2]=="s") {
         request = https;
     } else if(match[1]==undefined) {
         url = "http://" + url;
     }
 
-    console.log("URL: " + url);
+    console.log(`Got: ${url}`);
 
     request.get(url, res => {
-        telegram.sendMessage(msg.chat.id, url + " is up \u{1F604}");
+        console.log(`URL: ${url} || Status Code: ${res.statusCode}`);
+
+//      status code of client or server error
+        if(res.statusCode>=400 && res.statusCode<600) {
+            let message =  `${url} is responding, but the status code is ${res.statusCode} \u{1F633}`;
+            telegram.sendMessage(msg.chat.id, message);
+        } else {
+//      seems successful
+            telegram.sendMessage(msg.chat.id, url + " is up \u{1F604}");
+        }
     }).on('error', e => {
+//      is not responding
+        console.log(e);
         telegram.sendMessage(msg.chat.id, url + " is down \u{1F614}");
     });
 }
